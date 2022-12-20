68e8ba3c417b6b915732551d7cec0de80263c841
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2022-12-20 18:02:00 +0100

    finished game

--- game.py
+++ game.py
@@ -1,14 +1,25 @@
+from player import HumanPlayer,RandomComputerPlayer
+import time
 class Connect4:
     def __init__(self):
         #6 by 7 board
         self.board = [' ' for _ in range(6*7)]
         self.board_position = [i for i in range(6*7)]
+        self.current_winner = None
 
     def print_board(self):
-        for i in range(6):
-            row = self.board[6*i:6*i+7]
-            print('|'+'|'.join(row)+'|')
 
+        for i in range(6):
+            row = self.board[7*i:7*i+7]
+            print('|'+'|'.join(row)+'|')
+    @classmethod
+    def print_moves(cls):
+        print('|0|1|2|3|4|5|6|')
+    @classmethod
+    def print_board_nums(cls):
+        for i in range(6):
+            row = [str(j) for j in range(7*i,7*i+7)]
+            print('|'+'|'.join(row)+'|')
     def available_move(self):
         # should return a number from 0-6
         # giving the columns of availble moves
@@ -18,17 +29,8 @@ class Connect4:
     def can_play(self):
         return ' ' in self.board
 
-    def make_move(self,letter):
-        col_move = input('Choose a column (0-6):')
-        incorrect_move = True
-        while incorrect_move:
-            try:
-                int(col_move)
-                if col_move in self.available_move():
-                    incorrect_move = False
-            except:
-                print('The move was incorrect!')
-                col_move = input('Choose a column (0-6):')
+    def make_move(self,col_move,letter):
+        #col_move = input('Choose a column (0-6):')
 
         position = col_move
         for i in range(1,6):
@@ -38,43 +40,81 @@ class Connect4:
                 break
         self.board[position] = letter
 
-        self.is_winning_move(position,letter)
+        return position
+        #self.is_winning_move(position,letter)
 
     def is_winning_move(self,position,letter):
 
         #check in column
         col = position%7
-        if position<=14: #we can't pile 4 otherwise
+        if position<21: #we can't pile 4 otherwise
             if all([self.board[position+j*7]==letter for j in range(4)]):
                 return True
-
+        
         #check in row
-        row = postion//6
-        if any([all([self.board[i+j]==letter for j in range(4) if (i+j)//6==row]) for i in range(position-3,position+1) if i//6 == row]):
+        row = position//7
+
+        if any([all([self.board[i+j]==letter for j in range(4)] ) for i in range(position-3,position+1) if i//7==row and (i+3)//7==row]):
             return True
 
         #check in diagonal 1
         # diagonal 1 is [position -6, position , position+6] if row -1, row, row +1
 
-        for i in range(position-6*3,position,6):
-            if 0<=i<=41-4*6:
+        for i in range(position-6*4,position+1,6):
+            print(i)
+            if 3<=i<=20:#doesnt work for i outside
                 rows = []
                 result = []
 
                 for j in range(0,4*6,6):
-                    rows.append((i+j)//6)
+                    rows.append((i+j)//7)
                     result.append(self.board[i+j]==letter)
                 rows_set = set(rows)
-                if len(rows_set)==len(row)and all(result):
+                if len(rows_set)==len(rows)and all(result):
                     return True
 
 
         #check in diagonal 2
         # diagonal 2 is [position -8, position , position+8] if row -1, row, row +1
+        for i in range(position-8*4,position+1,8):
+            if 0<=i<=41-3*8:#doesnt work for i outside
+                rows = []
+                result = []
+
+                for j in range(0,4*8,8):
+                    rows.append((i+j)//7)
+                    result.append(self.board[i+j]==letter)
+                rows_set = set(rows)
+                if len(rows_set)==len(rows)and all(result):
+                    return True
+
+        return False
 
 
+def play(game,x_player,o_player,print_game=True):
+    if print_game:
+        Connect4.print_board_nums()
+        game.print_board()
+    letter = 'X'
+    current_player = x_player
+    while game.can_play():
+        move = current_player.get_move(game)
+        position = game.make_move(move,letter)
+        if game.is_winning_move(position,letter):
+            game.print_board()
+            print(f'Congrats! Player {letter} won the game!')
+            return
+        current_player = o_player if current_player == x_player else x_player
+        letter = current_player.letter
+        if print_game:
+            Connect4.print_moves()
+            game.print_board()
+        time.sleep(1)
 
+    print(f'It\'s a tie!')
 
-game = Connect4()
-game.print_board()
-print(game.available_move())
+if __name__ =='__main__':
+    game = Connect4()
+    x_player = HumanPlayer('X')
+    o_player = RandomComputerPlayer('O')
+    play(game,x_player,o_player,print_game=True)
--- player.py
+++ player.py
@@ -1,16 +1,35 @@
-
+import random
 class Player:
     def __init__(self,letter):
+        #letter is x or o
         self.letter = letter
 
 
 class HumanPlayer(Player):
     def __init__(self,letter):
-        super().__init__(self,letter)
+        super().__init__(letter)
 
-    def make_move(self):
+    def get_move(self,game):
+        col_move = input('Choose a column (0-6):')
+        incorrect_move = True
+        while incorrect_move:
+            try:
+                col_move = int(col_move)
+                if col_move not in game.available_move():
+                    raise ValueError
+            except ValueError:
+                print('The move was incorrect!')
+                col_move = input('Choose a column (0-6):')
+            else:
+                incorrect_move = False
+        return col_move
 
+        game.make_move(col_move)
 
 class RandomComputerPlayer(Player):
     def __init__(self,letter):
-        super().__init__(self,letter)
+        super().__init__(letter)
+
+    def get_move(self,game):
+        col_move = random.choice(game.available_move())
+        return col_move
